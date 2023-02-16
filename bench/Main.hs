147d1db8eea944324e1977df152aca0506b13cac
Ashutosh Pratap Singh <ashutosh.pratap.ext@juspay.in>
2023-02-16 15:06:26 +0530

    Added support for milliseconds parsing in text time

--- bench/Main.hs
+++ bench/Main.hs
@@ -10,13 +10,13 @@ import Data.Text as T
 main :: IO ()
 main = do
   let format = "%Y-%m-%dT%k:%M:%SZ"
-  let jformat = "%Y-%m-%dT%k:%M:%SZ"
-  let jformatq = "%Y-%m-%dT%k:%M:%S%QZ"
-  let ezFormat = "%Y-%m-%dT%k:%M:%S%Q%Ez"
+      jformat = "%Y-%m-%dT%k:%M:%SZ"
+      jformatq = "%Y-%m-%dT%k:%M:%S%QZ"
+      ezFormat = "%Y-%m-%dT%k:%M:%S%Q%Ez"
       dformat = "%d%m%Y"
-  let b str = parseTimeM True DTF.defaultTimeLocale format str :: Maybe UTCTime
-  let c str = parseTimeM True DTF.defaultTimeLocale (T.unpack ezFormat) str :: Maybe LocalTime
-  let x str = parseTimeM True DTF.defaultTimeLocale dformat str :: Maybe LocalTime
+      b str = parseTimeM True DTF.defaultTimeLocale (T.unpack jformatq) str :: Maybe UTCTime
+      c str = parseTimeM True DTF.defaultTimeLocale (T.unpack ezFormat) str :: Maybe LocalTime
+      x str = parseTimeM True DTF.defaultTimeLocale dformat str :: Maybe LocalTime
   defaultMain [ bench "fast parse time to Maybe UTCTime" $ nf (Time.parseTime jformat) "2022-10-02T12:22:32Z"
               , bench "parse time to Maybe UTCTime" $ nf b "2022-10-02T12:22:32Z"
               , bench "fast parse time with millis to Maybe UTCTime" $ nf (Time.parseTime jformatq) "2022-10-02T12:22:32.223Z"
@@ -25,4 +25,5 @@ main = do
               , bench "parse time to Maybe LocalTime" $ nf c "2020-09-10T16:01:41.395+05:30"
               , bench "parse day to Maybe UTCTime" $ nf x "25012023"
               , bench "fast parse day to Maybe UTCTime(%d%m%Y)" $ nf (Time.parseTime (T.pack dformat)) "25012023"
+              , bench "fast parse day to Maybe UTCTime(%Y/%-m/%-d)" $ nf (Time.parseTime "%Y/%-m/%-d") "2023/12/09"
               ]
