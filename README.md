286fdec2100ccb86f0b94936f254a4d77bd8d331
Ashutosh Pratap Singh <ashutosh.pratap.ext@juspay.in>
2023-04-06 14:32:38 +0530

    Updated the readme file along with the ghc-options

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
 # fast-time
 
-A custom parsers using flatparse for data-time.
+## A custom parsers using flatparse for data-time.
 
-## TODO
 
-- Need to add more foramts.
+### Supported Formats of Date & Time 
+#### Date-Time Formats  
+
+- "%Y-%m-%dT%k:%M:%SZ"
+- "%Y-%m-%dT%k:%M:%S%QZ"
+- "%Y-%m-%d %k:%M:%S%Q" 
+- "%Y-%m-%d %k:%M:%S" 
+- "%Y-%m-%dT%k:%M:%S%Q%Ez"
+
+#### Date Formats 
+
+- "%Y-%m-%d"
+- "%Y %m %d"
+- "%Y-%m %d"
+- "%Y %m-%d" 
+- "%Y/%-m/%-d"
+- "%d%m%Y"
+
+### Benchmarks 
+
+| Benchmark Name | Description | Data.Time Library | Fast-time | 
+| ----- | ----- | ----- | ----- | 
+| UTCTime | Converts the text time to UTCTime format. | 8.382 μs   (8.359 μs .. 8.425 μs) | 734.4 ns   (731.7 ns .. 736.4 ns) |
+| UTCTime Milliseconds | Converts the text time which includes millisecond to UTCTime format. | 11.28 μs   (11.24 μs .. 11.33 μs) | 806.6 ns   (804.4 ns .. 808.8 ns) | 
+| LocalTime | Converts the text time to Local time covering case of millisecond parsing as well. | 11.67 μs   (11.53 μs .. 11.81 μs | 1.200 μs   (1.180 μs .. 1.220 μs) | 
+| Date Parsing | Converts the text date in the given format. | 4.710 μs   (4.296 μs .. 5.211 μs) | 440.3 ns   (437.4 ns .. 443.6 ns) | 
+| Date Parsing ("%d%m%Y") | Converts the text date to the given format using isolate method of flatparse library. | 12.08 μs   (11.81 μs .. 12.29 μs) | 368.1 ns   (359.7 ns .. 378.0 ns) |
+
--- bench/Main.hs
+++ bench/Main.hs
@@ -14,16 +14,23 @@ main = do
       jformatq = "%Y-%m-%dT%k:%M:%S%QZ"
       ezFormat = "%Y-%m-%dT%k:%M:%S%Q%Ez"
       dformat = "%d%m%Y"
+      dformat2 = "%Y-%m-%d"
+      dformat3 = "%Y/%-m/%-d"
       b str = parseTimeM True DTF.defaultTimeLocale (T.unpack jformatq) str :: Maybe UTCTime
       c str = parseTimeM True DTF.defaultTimeLocale (T.unpack ezFormat) str :: Maybe LocalTime
-      x str = parseTimeM True DTF.defaultTimeLocale dformat str :: Maybe LocalTime
+      x str = parseTimeM True DTF.defaultTimeLocale dformat str :: Maybe UTCTime
+      y str = parseTimeM True DTF.defaultTimeLocale dformat2 str :: Maybe UTCTime 
+      z str = parseTimeM True DTF.defaultTimeLocale dformat3 str :: Maybe UTCTime
   defaultMain [ bench "fast parse time to Maybe UTCTime" $ nf (Time.parseTime jformat) "2022-10-02T12:22:32Z"
-              , bench "parse time to Maybe UTCTime" $ nf b "2022-10-02T12:22:32Z"
+              , bench "parse time to Maybe UTCTime using Data.Time package" $ nf b "2022-10-02T12:22:32Z"
               , bench "fast parse time with millis to Maybe UTCTime" $ nf (Time.parseTime jformatq) "2022-10-02T12:22:32.223Z"
+              , bench "parse time to Maybe millis UTCTime using Data.Time package" $ nf b "2022-10-02T12:22:32.223Z"
               , bench "fast parse day  to Maybe UTCTime" $ nf (Time.parseTime "%Y-%m %d") "2022-10-02"
+              , bench "parse day using the Data.Time package" $ nf y "2022-10-02"
               , bench "fast parse time to Maybe LocalTime" $ nf (Time.parseLTime ezFormat)  "2020-09-10T16:01:41.395+05:30"
-              , bench "parse time to Maybe LocalTime" $ nf c "2020-09-10T16:01:41.395+05:30"
-              , bench "parse day to Maybe UTCTime" $ nf x "25012023"
+              , bench "parse time to Maybe LocalTime using Data.Time package" $ nf c "2020-09-10T16:01:41.395+05:30"
               , bench "fast parse day to Maybe UTCTime(%d%m%Y)" $ nf (Time.parseTime (T.pack dformat)) "25012023"
+              , bench "parse day to Maybe UTCTime using Data.Time package" $ nf x "25012023"
               , bench "fast parse day to Maybe UTCTime(%Y/%-m/%-d)" $ nf (Time.parseTime "%Y/%-m/%-d") "2023/12/09"
+              , bench "parse day using Data.Time package" $ nf z "2023/12/09"
               ]
